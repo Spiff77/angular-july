4996882bc9e07947f57f60f38eb607a085a385c8
Thomas Lhomme <lhomme.thomas@gmail.com>
2023-07-05 09:09:14 +0200

    inputs

--- ecommerce/src/app/app.module.ts
+++ ecommerce/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
+import { MenuComponent } from './menu/menu.component';
+import { ProductComponent } from './product/product.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ProductComponent,
+    ProductListComponent
   ],
     imports: [
         BrowserModule,
