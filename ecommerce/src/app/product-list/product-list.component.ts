4dd3fcf869c187495716637885f94329419eedf0
Thomas Lhomme <lhomme.thomas@gmail.com>
2023-07-05 12:42:26 +0200

    ecommerce | ngFor + Filtering

--- ecommerce/src/app/product-list/product-list.component.ts
+++ ecommerce/src/app/product-list/product-list.component.ts
@@ -10,31 +10,31 @@ export class ProductListComponent {
 
   selectedProduct: Product | undefined
 
-  product1: Product = {
-    id: 10,
-    name: 'Frank Zappa',
-    category: 'Music',
-    active: true,
-    price: 20,
-    promo: .1,
-    description: "blablabla bla bla bla blabla bla"
-  }
-
-
-  product2: Product = {
-    id: 12,
-    name: 'Motorhead',
-    category: 'Music',
-    active: true,
-    price: 15,
-    promo: .1,
-    description: "bliblib blib bllibbliilb lblib"
-  }
-
-
+  filteringStr = ''
+  products: Product[] = [{
+      id: 10,
+      name: 'Frank Zappa',
+      category: 'Music',
+      active: true,
+      price: 20,
+      promo: .1,
+      description: "blablabla bla bla bla blabla bla"
+    }, {
+      id: 12,
+      name: 'Motorhead',
+      category: 'Music',
+      active: true,
+      price: 15,
+      promo: .1,
+      description: "bliblib blib bllibbliilb lblib"
+  }]
 
   receiveSelectedProduct(product: Product){
     this.selectedProduct = product
+  }
+
+  getProductsFiltered(): Product[]{
+    return this.products.filter(prod => prod.name.toLowerCase().includes(this.filteringStr.toLowerCase()))
   }
 
 }
