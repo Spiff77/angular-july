3cb4cf98e43e43556f6aeb37e9e54acb36e7083c
Thomas Lhomme <lhomme.thomas@gmail.com>
2023-07-05 09:48:53 +0200

    ecommerce  | input

--- ecommerce/src/app/product-list/product-list.component.ts
+++ ecommerce/src/app/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Product} from '../model/product.model';
 
 @Component({
   selector: 'app-product-list',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
+
+  product1: Product = {
+    id: 10,
+    name: 'Frank Zappa',
+    category: 'Music',
+    active: true,
+    price: 20,
+    promo: .1,
+    description: "blablabla bla bla bla blabla bla"
+  }
+
+
+  product2: Product = {
+    id: 12,
+    name: 'Motorhead',
+    category: 'Music',
+    active: true,
+    price: 15,
+    promo: .1,
+    description: "bliblib blib bllibbliilb lblib"
+  }
 
 }
