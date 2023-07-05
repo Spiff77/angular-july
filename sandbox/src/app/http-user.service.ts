7df684619cb85adfb45c3886d6d76acc4d7fff27
Thomas Lhomme <lhomme.thomas@gmail.com>
2023-07-05 16:28:05 +0200

    sandbox | environments

--- sandbox/src/app/http-user.service.ts
+++ sandbox/src/app/http-user.service.ts
@@ -2,15 +2,33 @@ import { Injectable } from '@angular/core';
 import {User} from './model/user.model';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {environment} from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpUserService {
 
+  private url = `${environment.base_api_url}users`
+
   constructor(private http: HttpClient) { }
 
   findAll(): Observable<User[]> {
-    return this.http.get<User[]>('https://jsonplaceholder.typicode.com/users')
+    return this.http.get<User[]>(this.url)
   }
 }
+/*
+- Environements
+ 1 - Générer l'environement: ng generate environments
+ 2 - Associer des valeurs dans ces environments (voir git - Sandbox)
+ 3 - Utiliser ces valeurs dans le service (voir git - Sandbox, HttpUserService)
+
+- deploy
+ 1 - installer http-server 'npm install -g http-server'
+ 2 - Build du projet grace à 'ng build --configuration=production'
+ 3 - Deplacement en console dans ./dist/[projectName]
+ 4 - Lancer http-server
+ 5 - Enjoy
+
+
+ */
---
+++ sandbox/src/environments/environment.development.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  base_api_url: 'https://jsonplaceholder.typicode.com/'
+};
