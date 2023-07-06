d8ab0d1a9a1bba3bd3d8162f0dd53b25b07f02db
Thomas Lhomme <lhomme.thomas@gmail.com>
2023-07-06 10:43:17 +0200

    DetailComponent

--- ecommerce/src/app/product-list/product-list.component.ts
+++ ecommerce/src/app/product-list/product-list.component.ts
@@ -29,11 +29,25 @@ export class ProductListComponent implements OnInit{
   }
 
 }
+
 /*
+  Créer le composent DetailComponent
+    product: Product
+    hint:
+      ngOnInit(): void {
+        let id = Number(this.activeRoute.snapshot.paramMap.get('id') ?? -1);
 
- /products --> ProductListComponent
- /home --> HomeComponent
- / --redirect--> /home
+        if(Number(id) >= 1){
+           this.productService.findOne(id).subscribe(p => this.product = p)
+       }
+  }
 
- Penser à updater le menu
+  Nouvelle methode dans le service ProductHttpService
+    findOne(id: number):  Observable<Product> -->http://localhost:8086/learn/api/products/[idInRouter(:id)]
+      http.get<Product>(...)
+
+  Ajouter une route dans AppModule
+    {path: 'products/:id', compoenent: DetailComponent}
+
+
  */
