5437259cd6c1f97f3dc018d751a37bf9600de9a8
Thomas Lhomme <lhomme.thomas@gmail.com>
2023-07-07 10:16:29 +0200

    sanbos | nested form

--- ecommerce/src/app/model/product.model.ts
+++ ecommerce/src/app/model/product.model.ts
@@ -1,9 +1,12 @@
+import {Supplier} from './supplier.model';
+
 export interface Product {
   id: number,
-  active: boolean,
-  name: string,
-  category: string,
-  description: string,
-  price: number,
-  promo: number
+    active: boolean,
+    name: string,
+    category: string,
+    description: string,
+    price: number,
+    promo: number,
+    supplier?: Supplier
 }
---
+++ ecommerce/src/app/model/supplier.model.ts
@@ -0,0 +1,6 @@
+export interface Supplier {
+  id: number,
+  companyName: string,
+  bankAccountNumber: string,
+  ca: number
+}
